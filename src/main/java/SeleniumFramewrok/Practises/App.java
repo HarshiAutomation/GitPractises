3393dc8f06360db882c96573ed41c842c0c252c2
HarshiAutomation <harshicr123@gmail.com>
2022-10-11 20:09:49 +0530

    Adding all files

---
+++ src/main/java/SeleniumFramewrok/Practises/App.java
@@ -0,0 +1,13 @@
+package SeleniumFramewrok.Practises;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
